6f4ee5b95aac78fe255a931b66c93fa09ebef44c
lakshay-0423 <blakshay162@gmail.com>
2026-07-25 15:11:43 +0530

    Time: 14 ms (56.6%), Space: 47.6 MB (82.98%) - LeetHub

---
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if(root==null) return "null";
+        StringBuilder sb=new StringBuilder();
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            TreeNode curr=q.poll();
+            if(curr==null){
+                sb.append("null,");
+                continue;
+            }
+            sb.append(curr.val).append(",");
+            q.add(curr.left);
+            q.add(curr.right);
+        }
+        sb.deleteCharAt(sb.length()-1);
+        return sb.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if(data.equals("null")) return null;
+        String[] arr=data.split(",");
+        Queue<TreeNode> q=new LinkedList<>();
+        TreeNode root=new TreeNode(Integer.parseInt(arr[0]));
+        q.add(root);
+        int i=1;
+        while(!q.isEmpty()){
+            TreeNode curr=q.poll();
+            if(!arr[i].equals("null")){
+                curr.left=new TreeNode(Integer.parseInt(arr[i]));
+                q.add(curr.left);
+            }
+            i++;
+            if(!arr[i].equals("null")){
+                curr.right=new TreeNode(Integer.parseInt(arr[i]));
+                q.add(curr.right);
+            }
+            i++;
+        }
+        return root;
+    }
+
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
